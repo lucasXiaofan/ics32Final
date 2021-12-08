d29661f7daaa59ebff9804d2948720483beebbed
sonakeshishyan <soke405@gmail.com>
2021-12-07 16:58:43 -0800

    comit

--- test_ds_messenger.py
+++ test_ds_messenger.py
@@ -5,7 +5,6 @@ import socket
 
 class TestDS_Messenger(unittest.TestCase):
     def test_is_true(self):
-        getting_token
         ds_messenger.DirectMessenger.token = "31292afb-8505-4421-b112-e18bc0938642"
         self.assertTrue(ds_messenger.DirectMessenger.send(ds_messenger.DirectMessenger, "Hello World", "Sped"))
 
